2706816d1601b628b2f060989a86158a91f07382
Dhananjay582 <dhananjaypatidar246@gmail.com>
2026-07-15 16:38:58 +0530

    Time: 990 ms (53.91%), Space: 35.6 MB (86.09%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -8,18 +8,7 @@ public:
             b = rem;
         }
         return a;
-    }
-    // int solve(int i,int seq1,int seq2,vector<int>& nums) {
-    //     if(i == nums.size()){
-    //         if(seq1 != 0 && seq2 != 0 && seq1 == seq2) return 1;
-    //         return 0;
-    //     }
-    //     if(dp[i][seq1][seq2] != -1) return dp[i][seq1][seq2];
-    //     int skip = solve(i+1,seq1,seq2,nums);
-    //     int take1 = solve(i+1,findgcd(seq1,nums[i]),seq2,nums);
-    //     int take2 = solve(i+1,seq1,findgcd(seq2,nums[i]),nums);
-    //     return dp[i][seq1][seq2] = (0LL + skip+take1+take2)%MOD;
-    // }   
+    }   
 public:
     int subsequencePairCount(vector<int>& nums) {
         int n = nums.size();
@@ -27,23 +16,25 @@ public:
         for(int i=0 ; i<n ; i++) {
             if(nums[i]>maxi) maxi = nums[i];
         }
-        int dp[n+1][maxi+1][maxi+1];
+        vector<vector<int>> prev(maxi+1,vector<int> (maxi+1,0));
+        vector<vector<int>> curr(maxi+1,vector<int> (maxi+1,0));
         for(int i=0 ; i<=maxi ; i++) {
             for(int j=0 ; j<=maxi ; j++) {
-                if(i!=0 && j!=0 && i==j) dp[n][i][j] = 1;
-                else dp[n][i][j] = 0;
+                if(i!=0 && j!=0 && i==j) prev[i][j] = 1;
+                else prev[i][j] = 0;
             }
         }
         for(int i=n-1 ; i>= 0 ; i--) {
             for(int j=maxi ; j>=0 ; j--) {
                 for(int k=maxi ; k>=0 ; k--) {
-                    int skip = dp[i+1][j][k];
-                    int take1 = dp[i+1][findgcd(j,nums[i])][k];
-                    int take2 = dp[i+1][j][findgcd(k,nums[i])];
-                    dp[i][j][k] = (0LL + skip + take1 + take2)%MOD; 
+                    int skip = prev[j][k];
+                    int take1 = prev[findgcd(j,nums[i])][k];
+                    int take2 = prev[j][findgcd(k,nums[i])];
+                    curr[j][k] = (0LL + skip + take1 + take2)%MOD; 
                 }
             }
+            prev = curr;
         }
-        return dp[0][0][0];
+        return prev[0][0];
     }
 };
